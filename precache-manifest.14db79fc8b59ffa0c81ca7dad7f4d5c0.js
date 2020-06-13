9244ef6d6ee992ff35cfb41dfc1a573e2982eef0
Daniel Intskirveli <dintskirveli@appnexus.com>
2020-06-13 09:40:07 -0400

    New build

---
+++ precache-manifest.14db79fc8b59ffa0c81ca7dad7f4d5c0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da2c31bd33ad5adb104999e2a24b6ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60d8996a957288158602",
+    "url": "/static/css/main.2b925783.chunk.css"
+  },
+  {
+    "revision": "592b5787e25d4522b98e",
+    "url": "/static/js/2.fce069f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fce069f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d8996a957288158602",
+    "url": "/static/js/main.dd5186ce.chunk.js"
+  },
+  {
+    "revision": "a249046128735d994a87",
+    "url": "/static/js/runtime-main.a88769cb.js"
+  },
+  {
+    "revision": "4bf4fb893e7d2660ae56d7097a9c9ebe",
+    "url": "/static/media/city.4bf4fb89.svg"
+  },
+  {
+    "revision": "0598daa99db03b2ca790ebb2ba5ab382",
+    "url": "/static/media/email.0598daa9.svg"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/static/media/github.1bb02710.svg"
+  },
+  {
+    "revision": "f211db737aaaaa6c537e69fc0b8282f6",
+    "url": "/static/media/pdf.f211db73.svg"
+  },
+  {
+    "revision": "a1b47ba453cf2607a986b4a1628f1d29",
+    "url": "/static/media/work.a1b47ba4.svg"
+  }
+]);
